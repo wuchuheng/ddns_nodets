acea28493107f66dfc207c78c07bcd310e2992d6
wuchuheng <root@wuchuheng.com>
2022-11-22 00:22:38 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ *  This file is part of emailNotes.
+ *
+ * @Description Say something for this file.
+ * @Author      wuchuheng<root@wuchuheng.com>
+ * @Time        2022/11/22 00:21
+ */
+
+const main = () => {
+    console.log("Hello world!!!")
+}
+
+export default main()
